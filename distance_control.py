36c7d03f6b044f72906b875511106ec28d1acbab
Surabhi Verma <surabhiverma96@gmail.com>
2017-12-23 20:22:38 +0530

    import custom message

--- distance_control.py
+++ distance_control.py
@@ -6,11 +6,12 @@
 # On work station:
 # python distance_control.py
 
+import roslib; roslib.load_manifest('ultrasonic_readings')
 import rospy
+from ultrasonic_readings.msg import UltrasonicReadings
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 
- 
 class DistanceControl():
 
     def __init__(self):
@@ -19,7 +20,7 @@ class DistanceControl():
 	    rospy.loginfo("To stop TurtleBot CTRL + C")    
             rospy.on_shutdown(self.shutdown)        
             self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-            self.move_cmd = Twist()           
+            self.move_cmd = Twist() # by default the Twist msg values are equal to 0     
             self.counter = 0
             self.kp = 0.1
             self.kd = 0.1
@@ -28,10 +29,14 @@ class DistanceControl():
             self.listener()                                
   
     def listener(self):
-            #rospy.init_node('listener', anonymous=True)
+
+            rospy.loginfo("listen")
             ti = rospy.get_time()
-            rospy.Subscriber("chatter", Float64, self.callback)
+            rospy.Subscriber("chatter", UltrasonicReadings, self.callback)
             rate = rospy.Rate(10);
+            if (self.counter == 1):
+                self.straight()
+
 
             while not rospy.is_shutdown():
                 self.cmd_vel.publish(self.move_cmd)
@@ -42,28 +47,34 @@ class DistanceControl():
 
     def callback(self, data): 
             #t = rospy.get_time() 
+            rospy.loginfo("callb")
+            self.start_x1 = data.data1 # setting reference position only for the front ultrasonic sensor
+            self.start_x2 = data.data2
+            error = self.start_x1 - self.start_x2 # positive error -> anticlk wise
 
-            if (self.counter == 0):
-                self.start = data.data # setting reference position 
-            self.counter = self.counter + 1
-            distance = data.data
-            error = distance - self.start 
-            self.straight()
-            #round_err = int(round((error))
-            #self.turn(error)
-            if (int(round(abs(error))) > 1):
+            #self.straight()
+
+            if (int(round(abs(error))) >= 1):
                self.turn(error)
                rospy.loginfo("turn") 
-               
+            if (self.counter == 0):
+               self.straight() 
+            self.counter = self.counter + 1
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
     def turn(self, error):
             self.move_cmd.linear.x = 0.1
-            self.move_cmd.angular.z = self.kp*error 
+            if (self.kp*error < 0.4):
+               self.move_cmd.angular.z = self.kp*error 
+            else:
+               self.move_cmd.angular.z = 0.4
+               rospy.loginfo("default speed")
 
     def straight(self):
             self.move_cmd.linear.x = 0.1
             self.move_cmd.angular.z = 0
 
+            
     def shutdown(self):
             rospy.loginfo("Stop TurtleBot")
 	    # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
