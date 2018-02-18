7be839ca87031c31e08f003a4339eff273418770
Surabhi Verma <surabhiverma96@gmail.com>
2018-02-19 00:27:11 +0530

    only useful contours(rectangles) used

--- red_detect.py
+++ red_detect.py
@@ -1,12 +1,16 @@
-# Last Edit: 18th Feb 2018
+# Last Edit: 19th Feb 2018
 # Author: Surabhi Verma 
 
 # ASSUMPTION1: Reqd. opencv 2.4.9 and python2
 # ASSUMPTION2: The image test4_color.jpeg is in the dataset folder. 
 # USAGE: python2 red_detect.py --image dataset/test4_color.jpeg
 
+# USAGE
+# python edge_detect_barcode.py --image images/test4_color.jpeg
+
 # import the necessary packages
 import numpy as np
+# to parse our command line arguments 
 import argparse
 import cv2
 import math
@@ -47,13 +51,13 @@ while True:
  
  # compute canny edges for closed boundaries with 100 as minvalue and 200 as maxvalue 
  edges = cv2.Canny(blur,100,200)
- #edges = cv2.dilate(edges, None, iterations = 1)
  # show the output of canny edge 
  cv2.imshow("edges", edges)
  # find the external contours  
  contours, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
  # draw bounding boxes for all the contours 
+ final_contours = []
  for ct in contours:
     # get the min area rect
     # rect[0] gives the (x,y) coordinates of center of rectangle 
@@ -64,19 +68,37 @@ while True:
     box = cv2.cv.BoxPoints(rect) 
     # convert all coordinates floating point values to int
     box = np.int0(box)
-    # draw a red 'nghien' rectangle
-    cv2.drawContours(image, [box], 0, (0, 0, 255))
-
- # calculating the moments to compute centroid of the contours 
- moments = [cv2.moments(cnt) for cnt in contours]
- # Rounding the centroids to integer
- centroids = [( int(round(m['m10']/m['m00'])),int(round(m['m01']/m['m00'])) ) for m in moments]
- # Drawing a black little empty circle in the centroid position for all contours 
- for c in centroids:
-    cv2.circle(image,c,5,(0,0,0))
-
- # show the final image 
- cv2.imshow("Image", image)
+    # find the area of the contour 
+    area = cv2.contourArea(ct)
+    # find the area of the bounding box
+    rect_area = rect[1][0]*rect[1][1]
+    # compute the ratio
+    extent = float(area)/rect_area
+    # This condition is applied so that only rectangles and not other noise is considered   
+    # if the ratio is almost = 1 
+    if (extent >= 0.8):
+       # draw a red 'nghien' rectangle
+       cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
+       # calculating the moments to compute centroid of the contours 
+       M = cv2.moments(ct) 
+       # Rounding the centroids to integer
+       centroids = ( int(round(M['m10']/M['m00'])), int(round(M['m01']/M['m00'])) )
+       # Drawing a black little empty circle in the centroid position for all contours
+       cv2.circle(image,centroids,5,(0,0,0),1)
+       # angle of the line to be drawn 
+       angle = rect[2];
+       # length of the line 
+       length = 750;
+       # start coordinates of the line 
+       startpt_x = int(round(rect[0][0]))
+       startpt_y = int(round(rect[0][1]))
+       # end coordinates of the line 
+       endpt_x = int(round(startpt_x + length * math.cos(angle * math.pi / 180.0)))
+       endpt_y = int(round(startpt_y + length * math.sin(angle * math.pi / 180.0)))
+       # draw line 
+       cv2.line(image,(startpt_x,startpt_y),(endpt_x,endpt_y),(255,0,0),10)
+       # show the final image 
+       cv2.imshow("Image", image)
 
  # repeat the program  
  c = cv2.waitKey(1)
