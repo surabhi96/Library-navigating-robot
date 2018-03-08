c7a585f733bdb4736366ee253410d9086ba0a521
Sakshi Kakde <kakdesakshi@gmail.com>
2018-03-08 22:54:15 +0530

    Update go_forward_avaoiding_obstacles.py

--- go_forward_avaoiding_obstacles.py
+++ go_forward_avaoiding_obstacles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Written by-Sakshi Kakde
-# Date-9 Dec 2017
+# Date-8 March 2017
 # A TurtleBot script that moves the Turtlebot if the distance measured by ultrasonuc sensor id less than 10 cm . Press CTRL + C to stop.  To run:
 # On TurtleBot:
 # roslaunch turtlebot_bringup minimal.launch
@@ -44,7 +44,62 @@ def MoveParallel():
     #SUBSCRIBER
     #Subscribes to topic 'chatter' to get distance 
     #the call back function is called when it some data id published on the topic chatter
-    rospy.Subscriber("/scan", LaserScan, callback)
+    rospy.Subscriber("#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+
+# For initial movement.
+move_cmd_init = Twist()
+move_cmd_init.linear.x = 0
+move_cmd_init.angular.z = 0
+
+
+cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+
+def callback(data):
+
+    global move_cmd
+    move_cmd= Twist()
+    if (data.ranges[320]>=0.4):
+      print "move"
+      move_cmd.linear.x = 0.2
+      move_cmd.angular.z =0
+    else:
+      print "stop"
+      move_cmd.linear.x = 0
+      move_cmd.angular.z =0
+
+    cmd_vel.publish(move_cmd)
+
+
+      
+   
+
+def shutdown():
+    # stop turtlebot
+    rospy.loginfo("Stop TurtleBot")
+    # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+    cmd_vel.publish(Twist())
+
+ 
+
+    
+def listener():
+    rospy.init_node('laserscan', anonymous=True)
+
+    rospy.Subscriber("scan", LaserScan, callback)
+     # spin() simply keeps python from exiting until this node is stopped
+    cmd_vel.publish(move_cmd_init)
+    rospy.on_shutdown(shutdown)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        listener()
+    except:
+        rospy.loginfo("GoForward node terminated.")/scan", LaserScan, callback)
 
 #
     # Initial movement.
@@ -63,4 +118,3 @@ if __name__ == '__main__':
         MoveParallel()
     except:
         rospy.loginfo("GoForward node terminated.")
-
