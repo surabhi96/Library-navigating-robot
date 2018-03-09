ff3a5ac42c0c8068fe61d858737bebc11ec707fa
Sakshi Kakde <kakdesakshi@gmail.com>
2018-03-09 08:43:56 +0530

    Update go_forward_avaoiding_obstacles.py

--- go_forward_avaoiding_obstacles.py
+++ go_forward_avaoiding_obstacles.py
@@ -2,48 +2,10 @@
 
 # Written by-Sakshi Kakde
 # Date-8 March 2017
-# A TurtleBot script that moves the Turtlebot forward. Also the bot will stop if there is some obstacle in front of it.
+# A TurtleBot script that moves the Turtlebot if the distance measured by ultrasonuc sensor id less than 10 cm . Press CTRL + C to stop.  To run:
+# On TurtleBot:
 # roslaunch turtlebot_bringup minimal.launch
-# On work station:
-# python move_paralle.py
 
-import rospy
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64
-import math
-
-# For initial movement.
-move_cmd_init = Twist()
-move_cmd_init.linear.x = 0
-move_cmd_init.angular.z = 0
-
-#PUBLISHER
-# Create a publisher which can "talk" to TurtleBot and tell it to move parallel
-cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-
-def callback(data):
-    distance=data.data
-    global move_cmd 
-    move_cmd= Twist()
-    # let's go forward at 0.2 m/s
-    move_cmd.linear.x = 0.1
-    # let's turn at 0 radians/s
-    move_cmd.angular.z = 0
-    if distance <10 :
-       rospy.loginfo("I published")
-       cmd_vel.publish(move_cmd)
-
-
-def MoveParallel():
-    # initiliaze
-    #By putting anonymous=False,by default, rospy registers signal handlers so that it can exit on Ctrl-C
-    #Refer-http://wiki.ros.org/rospy/Overview/Initialization%20and%20Shutdown#Initializing_your_ROS_Node
-    rospy.init_node('MoveParallel', anonymous=False)
-    print "Hi"
-    #SUBSCRIBER
-    #Subscribes to topic 'chatter' to get distance 
-    #the call back function is called when it some data id published on the topic chatter
-    rospy.Subscriber("#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -63,7 +25,7 @@ def callback(data):
     move_cmd= Twist()
     if (data.ranges[320]>=0.4):
       print "move"
-      move_cmd.linear.x = 0.2
+      move_cmd.linear.x = 0.1
       move_cmd.angular.z =0
     else:
       print "stop"
