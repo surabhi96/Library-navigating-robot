feac67e86dee87e3b3eb44ddf300b1cfbb032dbd
Surabhi Verma <surabhiverma96@gmail.com>
2018-05-21 11:19:10 +0530

    Publishes book QR and rack QR

--- zbar_pub.cpp
+++ zbar_pub.cpp
@@ -1,8 +1,12 @@
+// Publishes book QR and rack QR //
+
+// Editor: Surabhi Verma 
+// Last Edit: 21st May 2018
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+#include <std_msgs/Int8.h>
 #include <sstream>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -15,34 +19,33 @@ using namespace std;
 using namespace cv;
 using namespace zbar;
 
-// Global variable declaration:
 basic_string<char> name;
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "zbar_pub");
 	ros::NodeHandle n;
 	//The zbar publisher node will publish on the topic- BookID
-	ros::Publisher pub = n.advertise<std_msgs::String>("BookID", 1000);
+	ros::Publisher pub_book = n.advertise<std_msgs::String>("BookID", 1000);
+        ros::Publisher pub_rack = n.advertise<std_msgs::Int8>("Rack_num", 100);
 	ros::Rate loop_rate(10);
   	while (ros::ok())
   	{
 
-       Mat frame, frame_grayscale;
+        Mat frame, frame_grayscale;
  
-       // LOAD image
-       frame = imread("/home/surabhi/zbar_ws/src/zbar/src/rack1.png", CV_LOAD_IMAGE_COLOR);   // Read the file "image.jpg".
+        // LOAD image
+        frame = imread("/home/surabhi/zbar_ws/src/zbar/src/rack1.png", CV_LOAD_IMAGE_COLOR);   // Read the file "image.jpg".
  
-       if(! frame.data )  // Check for invalid input
-       {
+        if(! frame.data )  // Check for invalid input
+        {
               cout <<  "Could not open or find the image" << std::endl ;
               return -1;
-       }
+        }
 
-    ImageScanner scanner;
-    scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+        ImageScanner scanner;
+        scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
 
-//    for (;;) 
-//    {
         cvtColor(frame, frame_grayscale, CV_BGR2GRAY);
         int width = frame_grayscale.cols;
         int height = frame_grayscale.rows;
@@ -59,6 +62,7 @@ int main(int argc, char **argv)
             now = time(0);
             current = localtime(&now);
 
+            // store the qr decoded value in the global variable "name" 
             name = symbol->get_data();
             
             // Draw location of the symbols found
@@ -79,16 +83,34 @@ int main(int argc, char **argv)
                                                                                                                                                           
         // clean up
         image.set_data(NULL, 0);
-        //waitKey(27);
-//cout<<name<<endl;
-//    }
-    std_msgs::String msg;
-    msg.data = name;
-    
-    ROS_INFO("%s", msg.data.c_str());
-    pub.publish(msg);
-    ros::spinOnce();
-    loop_rate.sleep();
+
+        // message declaration 
+        std_msgs::String book_msg;
+        std_msgs::Int8 rack_msg;
+
+        // convert qr decoded data to integer data type 
+        // c_str() converts basic_string type to a C character array version of the string for atoi operation 
+        int input_type = 0;
+        input_type = atoi(name.c_str());
+
+        // If 'input_type' is equal to zero, it implies the decoded QR is that of a book 
+        if (input_type == 0) {
+          book_msg.data = name; 
+          ROS_INFO("book msg = %s", book_msg.data.c_str());
+          pub_book.publish(book_msg);
+        }
+  
+        // If 'input_type' is not equal to zero, it implies the decoded QR is that of a rack 
+        else {
+          rack_msg.data = input_type;
+          ROS_INFO("rack msg = %d", rack_msg.data); 
+          pub_rack.publish(rack_msg);
+        }  
+       
+        ros::spinOnce();
+        loop_rate.sleep();
+
   	}
-  	return 0;
+
+return 0;
 }
